85501fdba875ada981f65b642fa24ede579149d2
Reynold254 <reynold.burudi@student.moringaschool.com>
2025-03-18 14:22:12 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (task = "go to the office") {
+  return `This Monday, I will ${task}.`;
+};
+
+function wrapAdjective(wrapper = "*") {
+  return function (adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
+// Test cases
+console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("go swimming")); // "This Saturday, I want to go swimming!"
+
+console.log(mondayWork()); // "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!!"
